ec7b4d77c9999d67d102de5beff5bceefc9a5c7b
Abhinav Kumar Rao <shashankr212@gmail.com>
2025-12-30 13:27:22 +0530

    comments removed

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -6,18 +6,17 @@ import db from "./../db/index.js";
 const isValidEmail = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 
 export const signup = async (req, res) => {
-  console.log("trying to sign up");
+  //   console.log("trying to sign up");
 
-  console.log("printng db", db);
+  //   console.log("printng db", db);
 
-  console.log(db);
+  //   console.log(db);
   const { full_name, email, password } = req.body;
 
-  console.log(req.body);
-  console.log("password is ", password);
-  console.log("password type is ", typeof password);
+  //   console.log(req.body);
+  //   console.log("password is ", password);
+  //   console.log("password type is ", typeof password);
 
-  // 1. Validation [cite: 37, 38]
   if (!full_name || !email || !password) {
     return res.status(400).json({ error: "All fields are required" });
   }
@@ -31,32 +30,31 @@ export const signup = async (req, res) => {
   }
 
   try {
-    // 2. Check if user exists
-    console.log("inside try");
-    console.log("prining type of email", typeof email);
+    // Checking if user exists
+    // console.log("inside try");
+    // console.log("prining type of email", typeof email);
 
     const rows = await db.query("SELECT * FROM users");
-    console.log("printing rows", rows);
+    // console.log("printing rows", rows);
 
     const userExist = await db.query("SELECT * FROM users WHERE email = $1", [
       email,
     ]);
 
-    console.log("logging db rows");
+    // console.log("logging db rows");
 
-    console.log(db.rows);
+    // console.log(db.rows);
 
-    console.log(userExist);
+    // console.log(userExist);
 
-    console.log("logging userrows");
+    // console.log("logging userrows");
 
-    console.log(userExist.rows);
+    // console.log(userExist.rows);
 
     if (userExist.rows.length > 0) {
       return res.status(400).json({ error: "Email already exists" });
     }
 
-    // 3. Hash Password [cite: 53]
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
@@ -65,9 +63,8 @@ export const signup = async (req, res) => {
       "INSERT INTO users (full_name, email, password_hash) VALUES ($1, $2, $3) RETURNING id, full_name, email, role",
       [full_name, email, hashedPassword]
     );
-    console.log("user added", newUser);
+    // console.log("user added", newUser);
 
-    // 5. Generate Token [cite: 39]
     const token = jwt.sign(
       { id: newUser.rows[0].id, role: newUser.rows[0].role },
       process.env.JWT_SECRET,
@@ -94,6 +91,17 @@ export const login = async (req, res) => {
       return res.status(400).json({ error: "Invalid email or password" });
     }
     const user = userQuery.rows[0];
+
+    
+    // console.log("logging db rows");
+
+    // console.log(db.rows);
+
+    // console.log(userExist);
+
+    // console.log("logging userrows");
+
+    // console.log(userExist.rows);
 
     // 2. Verify Password [cite: 41]
     const validPass = await bcrypt.compare(password, user.password_hash);
--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -2,18 +2,17 @@ import pg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
-console.log("printing ditenv");
+// console.log("printing ditenv");
 
-console.log(dotenv.config());
-console.log("DEBUG: My Database URL is:", String(process.env.DATABASE_URL));
+// console.log(dotenv.config());
+// console.log("DEBUG: My Database URL is:", String(process.env.DATABASE_URL));
 
 const pool = new pg.Pool({
   connectionString: String(process.env.DATABASE_URL),
-  // Uncomment the line below if deploying to Render/Neon (Cloud DB)
   // ssl: { rejectUnauthorized: false }
 });
 
-console.log("pool is ", pool);
+// console.log("pool is ", pool);
 
 export default {
   query: (text, params) => pool.query(text, params),
