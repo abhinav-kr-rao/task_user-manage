5ce504dd3faf28b9a2a9391bc22466eee94943eb
Abhinav Kumar Rao <shashankr212@gmail.com>
2025-12-30 13:20:37 +0530

    signing up error fixed

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -2,6 +2,10 @@ import pg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
+console.log("printing ditenv");
+
+console.log(dotenv.config());
+console.log("DEBUG: My Database URL is:", String(process.env.DATABASE_URL));
 
 const pool = new pg.Pool({
   connectionString: String(process.env.DATABASE_URL),
